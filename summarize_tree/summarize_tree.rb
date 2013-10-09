2d01e68ea3c5e90f2c010ecd8453dfa4326b9cc3
Travis Starkson <stark451@serenity.morris.umn.edu>
2013-10-08 22:28:08 -0500

    forgot one thing

--- summarize_tree/summarize_tree.rb
+++ summarize_tree/summarize_tree.rb
@@ -4,4 +4,4 @@ dir = ARGV[0]
 puts "Number of files:"
 puts Dir[File.join(dir, '**', '*')].count { |file| File.file?(file) }
 puts "Number of directories:"
-puts (Dir["**/*"].length -  Dir[File.join(dir, '**', '*')].count { |file| File.file?(file) } + 1)
+puts (Dir["**/*"].length -  Dir[File.join(dir, '**', '*')].count { |file| File.file?(file) })
